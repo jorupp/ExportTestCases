09399de4d7caec552c4de2b720bab85611c4de33
Jonathan Rupp <jorupp@gmail.com>
2014-09-02 10:43:42 -0500

    Add 'workitem' column and fix the way test cases are loaded for a test suite

--- TestCaseExport/Exporter.cs
+++ TestCaseExport/Exporter.cs
@@ -19,25 +19,27 @@ namespace TestCaseExport
             using (var pkg = new ExcelPackage())
             {
                 var sheet = pkg.Workbook.Worksheets.Add("Test Script");
-                sheet.Cells[1, 1].Value = "Test Condition";
-                sheet.Cells[1, 2].Value = "Action/Description";
-                sheet.Cells[1, 3].Value = "Input Data";
-                sheet.Cells[1, 4].Value = "Expected Result";
-                sheet.Cells[1, 5].Value = "Actual Result";
-                sheet.Cells[1, 6].Value = "Pass/Fail";
-                sheet.Cells[1, 7].Value = "Comments";
+                sheet.Cells[1, 1].Value = "Work Item ID";
+                sheet.Cells[1, 2].Value = "Test Condition";
+                sheet.Cells[1, 3].Value = "Action/Description";
+                sheet.Cells[1, 4].Value = "Input Data";
+                sheet.Cells[1, 5].Value = "Expected Result";
+                sheet.Cells[1, 6].Value = "Actual Result";
+                sheet.Cells[1, 7].Value = "Pass/Fail";
+                sheet.Cells[1, 8].Value = "Comments";
 
                 sheet.Column(1).Width = 15;
-                sheet.Column(2).Width = 50;
-                sheet.Column(3).Width = 15;
-                sheet.Column(4).Width = 50;
+                sheet.Column(2).Width = 15;
+                sheet.Column(3).Width = 50;
+                sheet.Column(4).Width = 15;
                 sheet.Column(5).Width = 50;
-                sheet.Column(6).Width = 15;
-                sheet.Column(7).Width = 20;
+                sheet.Column(6).Width = 50;
+                sheet.Column(7).Width = 15;
+                sheet.Column(8).Width = 20;
 
 
                 int row = 2;
-                foreach (var testCase in testSuite.TestCases.Select(i => i.TestCase))
+                foreach (var testCase in testSuite.AllTestCases)
                 {
                     var replacementSets = GetReplacementSets(testCase);
                     foreach (var replacements in replacementSets)
@@ -49,21 +51,27 @@ namespace TestCaseExport
                         }
                         if (firstRow != row)
                         {
-                            var merged = sheet.Cells[firstRow, 1, row - 1, 1];
-                            merged.Merge = true;
-                            CleanupText(merged, testCase.Title, replacements);
-                            merged.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                            merged.Style.VerticalAlignment = ExcelVerticalAlignment.Top;
+                            var mergedID = sheet.Cells[firstRow, 1, row - 1, 1];
+                            mergedID.Merge = true;
+                            mergedID.Value = testCase.WorkItem == null ? "" : testCase.WorkItem.Id.ToString();
+                            mergedID.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            mergedID.Style.VerticalAlignment = ExcelVerticalAlignment.Top;
+
+                            var mergedText = sheet.Cells[firstRow, 2, row - 1, 2];
+                            mergedText.Merge = true;
+                            CleanupText(mergedText, testCase.Title, replacements);
+                            mergedText.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                            mergedText.Style.VerticalAlignment = ExcelVerticalAlignment.Top;
                         }
                     }
                 }
 
-                var header = sheet.Cells[1, 1, 1, 7];
+                var header = sheet.Cells[1, 1, 1, 8];
                 header.Style.Font.Bold = true;
                 header.Style.Fill.PatternType = ExcelFillStyle.Solid;
                 header.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(255, 226, 238, 18));
 
-                sheet.Cells[1, 1, row, 7].Style.WrapText = true;
+                sheet.Cells[1, 1, row, 8].Style.WrapText = true;
 
                 pkg.SaveAs(new FileInfo(filename));
             }
@@ -91,8 +99,8 @@ namespace TestCaseExport
             var sharedRef = testAction as ISharedStepReference;
             if (null != testStep)
             {
-                CleanupText(xlWorkSheet.Cells[row, 2], testStep.Title.ToString(), replacements);
-                CleanupText(xlWorkSheet.Cells[row, 4], testStep.ExpectedResult.ToString(), replacements);
+                CleanupText(xlWorkSheet.Cells[row, 3], testStep.Title.ToString(), replacements);
+                CleanupText(xlWorkSheet.Cells[row, 5], testStep.ExpectedResult.ToString(), replacements);
             }
             else if (null != group)
             {
